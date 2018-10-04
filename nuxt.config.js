035e34ed6dddbb0446c1288efde3423a231538de
lucaspatel <lucaspatel@gmail.com>
2018-10-04 13:36:14 -0700

    desc updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Undergraduate Bioinformatics Club at UC San Diego' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
